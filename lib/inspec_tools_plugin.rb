2e62b3d7a3a9bdd9a5d544e2aa514e951b083df9
Kevin J. Smith <kevin.j.smith2@cerner.com>
2019-08-22 15:08:17 -0500

    Removing inspec tools plugin logic from another PR
    Signed-off-by: Kevin J. Smith <kevin.j.smith2@cerner.com>

--- lib/inspec_tools_plugin.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-libdir = File.dirname(__FILE__)
-$LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
-
-require 'inspec_tools/version'
-require 'inspec_tools/plugin'
-
-require 'rubygems'
