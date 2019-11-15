455a8552cdec718a6c9731a89d626ee9921f6d12
lukemalinowski <40170735+lukemalinowski@users.noreply.github.com>
2019-11-15 12:37:05 -0500

    Fixes #77 by shifting help commands around (#87)
    * Fixes #77 by shifting help commands around
    
    Signed-off-by: Luke Malinowski <lmalinowski@mitre.org>
    
    * Fix codefactor issues to make an assignment more clear
    
    Signed-off-by: Luke Malinowski <lmalinowski@mitre.org>

--- lib/inspec_tools/plugin_cli.rb
+++ lib/inspec_tools/plugin_cli.rb
@@ -245,4 +245,13 @@ end
 #---------------------------------------------------------------------#
 ARGV.push("--log-directory=#{Dir.pwd}/logs") if (log_commands & ARGV).empty? && (help_commands & ARGV).empty?
 
+# Push help to front of command so thor recognizes subcommands are called with help
+if help_commands.any? { |cmd| ARGV.include? cmd }
+  help_commands.each do |cmd|
+    if (match = ARGV.delete(cmd))
+      ARGV.unshift match
+    end
+  end
+end
+
 # rubocop:enable Style/GuardClause
