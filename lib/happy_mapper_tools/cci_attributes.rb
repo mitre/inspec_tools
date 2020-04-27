9610264afb6d321d59649ae9ba33b009d45499f1
Aaron Lippold <lippold@gmail.com>
2020-04-27 17:41:49 -0400

    cce data fix until we know what do do with it
    Signed-off-by: Aaron Lippold <lippold@gmail.com>

--- lib/happy_mapper_tools/cci_attributes.rb
+++ lib/happy_mapper_tools/cci_attributes.rb
@@ -64,9 +64,16 @@ module HappyMapperTools
 
       def fetch_nists(ccis)
         ccis = [ccis] unless ccis.is_a?(Array)
+
+        # some of the XCCDF files were having CCE- tags show up which 
+        # we don't support, not sure if this is a typo on their part or 
+        # we need to see about supporting CCE tags but ... for now
+         
+        filtered_ccis = ccis.select { |f| /CCI-/.match(f) }
         nists = []
         nist_ver = cci_items[0].cci_item[0].references.references.max_by(&:version).version
-        ccis.each do |cci|
+        filtered_ccis.each do |cci|
+        # require 'pry'; require 'pry-nav'; binding.pry
           nists << cci_items[0].cci_item.select { |item| item.id == cci }.first.references.references.max_by(&:version).index
         end
         nists << ('Rev_' + nist_ver)
