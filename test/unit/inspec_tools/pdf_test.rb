f44aa30261edbb650f404ab06d559968e4277329
Kyle Fagan <kfagan@mitre.org>
2020-06-18 14:37:28 -0400

    Respect debug env variable when running tests

--- test/unit/inspec_tools/pdf_test.rb
+++ test/unit/inspec_tools/pdf_test.rb
@@ -7,17 +7,17 @@ class PDFTest < Minitest::Test
 
   def test_pdf_init_with_valid_params
     pdf = File.open('examples/CIS_Ubuntu_Linux_16.04_LTS_Benchmark_v1.0.0.pdf')
-    assert(InspecTools::PDF.new(pdf, 'test', false))
+    assert(InspecTools::PDF.new(pdf, 'test', ENV['INSPEC_TOOLS_DEBUG'] || false))
   end
 
   def test_pdf_init_with_invalid_params
     pdf = nil
-    assert_raises(StandardError) { InspecTools::PDF.new(pdf, 'test', false) }
+    assert_raises(StandardError) { InspecTools::PDF.new(pdf, 'test', ENV['INSPEC_TOOLS_DEBUG'] || false) }
   end
 
   def test_pdf_to_inspec
     pdf = File.open('examples/CIS_Ubuntu_Linux_16.04_LTS_Benchmark_v1.0.0.pdf')
-    pdf_tool = InspecTools::PDF.new(pdf, 'test', true)
+    pdf_tool = InspecTools::PDF.new(pdf, 'test', ENV['INSPEC_TOOLS_DEBUG'] || false)
     inspec_json = pdf_tool.to_inspec
     assert(inspec_json)
   end
