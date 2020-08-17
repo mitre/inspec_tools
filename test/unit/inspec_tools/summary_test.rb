7531b0a46b4dde93ad414fe6a6a57037433ea540
Kyle Fagan <kfagan@mitre.org>
2020-08-17 16:32:39 -0400

    Remove deprecation warnings

--- test/unit/inspec_tools/summary_test.rb
+++ test/unit/inspec_tools/summary_test.rb
@@ -11,20 +11,20 @@ class SummaryTest < Minitest::Test
   def test_summary_init_with_valid_params
     options = { options: {} }
     options[:options][:inspec_json] = 'examples/sample_json/rhel-simp.json'
-    assert(InspecTools::Summary.new(options))
+    assert(InspecTools::Summary.new(**options))
   end
 
   def test_summary_init_with_invalid_params
     # File not found
     options = { options: {} }
     options[:options][:inspec_json] = 'does_not_exist'
-    assert_raises(StandardError) { InspecTools::Summary.new(options) }
+    assert_raises(StandardError) { InspecTools::Summary.new(**options) }
   end
 
   def test_inspec_to_summary
     options = { options: {} }
     options[:options][:inspec_json] = 'examples/sample_json/rhel-simp.json'
-    inspec_tools = InspecTools::Summary.new(options)
+    inspec_tools = InspecTools::Summary.new(**options)
     inspec_tools.to_summary
     assert_equal(77, inspec_tools.summary[:compliance])
     assert_equal(33, inspec_tools.summary[:status][:failed][:medium])
@@ -34,7 +34,7 @@ class SummaryTest < Minitest::Test
     options = { options: {} }
     options[:options][:inspec_json] = 'examples/sample_json/rhel-simp.json'
     options[:options][:threshold_inline] = '{compliance.min: 77, failed.critical.max: 0, failed.high.max: 3}'
-    inspec_tools = InspecTools::Summary.new(options)
+    inspec_tools = InspecTools::Summary.new(**options)
     assert_output(%r{Compliance threshold of \d\d% met}) { inspec_tools.threshold }
   end
 
@@ -42,16 +42,16 @@ class SummaryTest < Minitest::Test
     options = { options: {} }
     options[:options][:inspec_json] = 'examples/sample_json/rhel-simp.json'
     options[:options][:threshold_inline] = '{compliance.min: 80, failed.critical.max: 0, failed.high.max: 0}'
-    inspec_tools = InspecTools::Summary.new(options)
+    inspec_tools = InspecTools::Summary.new(**options)
     assert_output(%r{Expected compliance.min:80 got:77(\r\n|\r|\n)Expected failed.high.max:0 got:3}) { inspec_tools.threshold }
 
     options[:options][:threshold_inline] = '{compliance.max: 50}'
-    inspec_tools = InspecTools::Summary.new(options)
+    inspec_tools = InspecTools::Summary.new(**options)
     assert_output(%r{Expected compliance.max:50 got:77}) { inspec_tools.threshold }
 
     options[:options][:threshold_file] = 'examples/sample_yaml/threshold.yaml'
     options[:options][:threshold_inline] = nil
-    inspec_tools = InspecTools::Summary.new(options)
+    inspec_tools = InspecTools::Summary.new(**options)
     assert_output(%r{Expected compliance.min:81 got:77}) { inspec_tools.threshold }
   end
 
@@ -60,7 +60,7 @@ class SummaryTest < Minitest::Test
     options[:options][:inspec_json] = 'examples/sample_json/rhel-simp.json'
     options[:options][:threshold_inline] = '{compliance.min: 77}'
     options[:options][:threshold_file] = 'examples/sample_yaml/threshold.yaml'
-    inspec_tools = InspecTools::Summary.new(options)
+    inspec_tools = InspecTools::Summary.new(**options)
     assert(inspec_tools.threshold)
 
     inspec_tools.to_summary
