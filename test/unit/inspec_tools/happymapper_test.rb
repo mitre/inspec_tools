ef93832267a025a84c7c49f20c33f8e63ce16013
Robert Clark <rbclark@mitre.org>
2021-04-23 11:29:23 -0400

    Cleanup Ident test to use hash instead of array
    Signed-off-by: Robert Clark <rbclark@mitre.org>

--- test/unit/inspec_tools/happymapper_test.rb
+++ test/unit/inspec_tools/happymapper_test.rb
@@ -3,19 +3,19 @@ require 'minitest/autorun'
 
 describe "HappyMapperTools::Benchmark::Ident correctly determines the system for each identifier" do
   # test values (tv); tv[0] == identifier, tv[1] == system
-  tvList = [
-    ['CCI-000213',  'http://cyber.mil/cci'],
-    ['V-72859',     'http://cyber.mil/legacy'],
-    ['SV-87511',    'http://cyber.mil/legacy'],
-    ['CCI-00213',   'https://public.cyber.mil/stigs/cci/'],
-    ['CCI-0000213',   'https://public.cyber.mil/stigs/cci/'],
-  ]
+  tvList = {
+    'CCI-000213' => 'http://cyber.mil/cci',
+    'V-72859' =>    'http://cyber.mil/legacy',
+    'SV-87511' =>   'http://cyber.mil/legacy',
+    'CCI-00213' =>  'https://public.cyber.mil/stigs/cci/',
+    'CCI-0000213' =>  'https://public.cyber.mil/stigs/cci/',
+  }
 
-  tvList.each do |tv|
-    it tv[0] do
+  tvList.each do |identifier, system|
+    it identifier do
       # Ident.new automatically determines ident.system
-      ident = HappyMapperTools::Benchmark::Ident.new tv[0]
-      assert_equal(tv[1], ident.system)
+      ident = HappyMapperTools::Benchmark::Ident.new identifier
+      assert_equal(system, ident.system)
     end
   end
 end
